9893c19ee2fb8d3ad0d8cfdd4cab4e2dadc252da
Kirill Ushnurtsev <ushnurtsev@gmail.com>
2020-08-26 21:28:11 +0400

    Initial commit

---
+++ array_manipulation/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the arrayManipulation function below.
+func arrayManipulation(n int32, queries [][]int32) int64 {
+	arr := make([]int64, n)
+	var max int64
+	for _, row := range queries {
+		a, b, k := int(row[0]), int(row[1]), int64(row[2])
+		arr[a-1] += k
+		if b < int(n) {
+			arr[b] -= k
+		}
+	}
+
+	var sum int64
+	for _, e := range arr {
+		sum += e
+		if sum > max {
+			max = sum
+		}
+	}
+	return max
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nm := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nm[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	mTemp, err := strconv.ParseInt(nm[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	var queries [][]int32
+	for i := 0; i < int(m); i++ {
+		queriesRowTemp := strings.Split(readLine(reader), " ")
+
+		var queriesRow []int32
+		for _, queriesRowItem := range queriesRowTemp {
+			queriesItemTemp, err := strconv.ParseInt(queriesRowItem, 10, 64)
+			checkError(err)
+			queriesItem := int32(queriesItemTemp)
+			queriesRow = append(queriesRow, queriesItem)
+		}
+
+		if len(queriesRow) != int(3) {
+			panic("Bad input")
+		}
+
+		queries = append(queries, queriesRow)
+	}
+
+	result := arrayManipulation(n, queries)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}

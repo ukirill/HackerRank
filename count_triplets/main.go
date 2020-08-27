6b1d44abd84841d5c4222969dd303353618bdbca
Kirill Ushnurtsev <ushnurtsev@gmail.com>
2020-08-27 21:46:40 +0400

    Count Triplets

---
+++ count_triplets/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the countTriplets function below.
+func countTriplets(arr []int64, r int64) int64 {
+	const tupleLen = 3
+	combinationsByLen := [tupleLen]map[int64]int64{
+		{},
+		{},
+		{},
+	}
+	for _, n := range arr {
+		if n%r == 0 {
+			for i := tupleLen - 2; i >= 0; i-- {
+				if c, ok := combinationsByLen[i][n/r]; ok {
+					combinationsByLen[i+1][n] += c
+				}
+			}
+		}
+		combinationsByLen[0][n] += 1
+	}
+	var sum int64
+	for _, s := range combinationsByLen[tupleLen-1] {
+		sum += s
+	}
+	return sum
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nr := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(nr[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	r, err := strconv.ParseInt(nr[1], 10, 64)
+	checkError(err)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int64
+
+	for i := 0; i < int(n); i++ {
+		arrItem, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arr = append(arr, arrItem)
+	}
+
+	ans := countTriplets(arr, r)
+
+	fmt.Fprintf(writer, "%d\n", ans)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
